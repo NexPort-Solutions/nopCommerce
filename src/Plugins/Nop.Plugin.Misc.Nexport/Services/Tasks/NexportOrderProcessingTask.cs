b8539f9e41c8789fa0d139df8962ddc8e24edf43
Thai Nguyen <thai.nguyen@nexportengineering.com>
2019-08-28 11:35:51 -0500

    Fix order processing task
    Handle missing information and add additional logging information

--- src/Plugins/Nop.Plugin.Misc.Nexport/Services/Tasks/NexportOrderProcessingTask.cs
+++ src/Plugins/Nop.Plugin.Misc.Nexport/Services/Tasks/NexportOrderProcessingTask.cs
@@ -68,16 +68,14 @@ namespace Nop.Plugin.Misc.Nexport.Services.Tasks
 
         public void Execute()
         {
-            //var pluginDescriptor = _pluginService.GetPluginDescriptorBySystemName<IPlugin>("Misc.Nexport", LoadPluginsMode.All);
-
             if (!_widgetPluginManager.IsPluginActive("Misc.Nexport"))
                 return;
 
             try
             {
                 var orders = (from q in _nexportOrderProcessingQueueRepository.Table
-                    orderby q.UtcDateCreated
-                    select q.Id).Take(_batchSize).ToList();
+                              orderby q.UtcDateCreated
+                              select q.Id).Take(_batchSize).ToList();
 
                 ProcessNexportOrders(orders);
             }
@@ -91,89 +89,143 @@ namespace Nop.Plugin.Misc.Nexport.Services.Tasks
         {
             try
             {
-                foreach (var orderId in queueItemIds)
+                foreach (var queueItemId in queueItemIds)
                 {
                     try
                     {
-                        _logger.Information($"Begin processing order {orderId}");
+                        _logger.Information($"Begin processing order processing queue item {queueItemId}");
 
-                        var queueItem = _nexportOrderProcessingQueueRepository.GetById(orderId);
+                        var queueItem = _nexportOrderProcessingQueueRepository.GetById(queueItemId);
+
+                        if (queueItem == null)
+                            continue;
+
                         var order = _orderService.GetOrderById(queueItem.OrderId);
-                        var userMapping = _nexportService.FindUserMappingByCustomerId(order.CustomerId);
 
-                        var store = _storeService.GetStoreById(order.StoreId);
-                        var orgId = _genericAttributeService.GetAttribute<Guid>(store, "NexportSubscriptionOrganizationId", store.Id);
-
-                        if (orgId == Guid.Empty)
+                        if (order != null)
                         {
-                            orgId = _nexportSettings.RootOrganizationId.Value;
-                        }
+                            var userMapping = _nexportService.FindUserMappingByCustomerId(order.CustomerId);
 
-                        var invoiceId = _nexportService.BeginNexportOrderInvoiceTransaction(orgId, userMapping.NexportUserId);
-
-                        decimal invoiceTotalCost = 0;
-
-                        var redeemingOrderIds = new List<int>();
-
-                        foreach (var orderItem in order.OrderItems)
-                        {
-                            var mapping = _nexportService.GetProductMappingByNopProductId(orderItem.ProductId);
-                            if (mapping != null)
+                            if (userMapping != null)
                             {
-                                var productCost = orderItem.Product.ProductCost;
-                                var subscriptionOrgId = mapping.NexportSubscriptionOrgId ?? orgId;
-                                var groupMembershipIds =
-                                    _nexportService.GetProductGroupMembershipIds(mapping.NopProductId);
-                                string invoiceItemId;
+                                var store = _storeService.GetStoreById(order.StoreId);
 
-                                if (mapping.Type == NexportProductTypeEnum.Catalog)
+                                if (store != null)
                                 {
-                                    invoiceItemId = _nexportService.AddItemToNexportOrderInvoice(invoiceId, mapping.NexportCatalogId,
-                                        CreateInvoiceItemRequest.ProductTypeEnum.Catalog, productCost, subscriptionOrgId, groupMembershipIds, mapping.UtcAccessExpirationDate, mapping.AccessTimeLimit.ToString());
-                                } else
-                                {
-                                    invoiceItemId =_nexportService.AddItemToNexportOrderInvoice(invoiceId, mapping.NexportCatalogSyllabusLinkId.Value,
-                                        CreateInvoiceItemRequest.ProductTypeEnum.Syllabus, productCost, subscriptionOrgId, groupMembershipIds, mapping.UtcAccessExpirationDate, mapping.AccessTimeLimit.ToString());
+                                    var orgId = _genericAttributeService.GetAttribute<Guid>(store,
+                                        "NexportSubscriptionOrganizationId", store.Id);
+
+                                    if (orgId == Guid.Empty)
+                                    {
+                                        orgId = _nexportSettings.RootOrganizationId.Value;
+                                    }
+
+                                    var invoiceId =
+                                        _nexportService.BeginNexportOrderInvoiceTransaction(orgId,
+                                            userMapping.NexportUserId);
+
+                                    decimal invoiceTotalCost = 0;
+
+                                    var redeemingOrderIds = new List<int>();
+
+                                    foreach (var orderItem in order.OrderItems)
+                                    {
+                                        var mapping =
+                                            _nexportService.GetProductMappingByNopProductId(orderItem.ProductId);
+                                        if (mapping != null)
+                                        {
+                                            var productCost = orderItem.Product.ProductCost;
+                                            var subscriptionOrgId = mapping.NexportSubscriptionOrgId ?? orgId;
+                                            var groupMembershipIds =
+                                                _nexportService.GetProductGroupMembershipIds(mapping.NopProductId);
+                                            string invoiceItemId;
+
+                                            if (mapping.Type == NexportProductTypeEnum.Catalog)
+                                            {
+                                                invoiceItemId = _nexportService.AddItemToNexportOrderInvoice(invoiceId,
+                                                    mapping.NexportCatalogId,
+                                                    CreateInvoiceItemRequest.ProductTypeEnum.Catalog, productCost,
+                                                    subscriptionOrgId, groupMembershipIds,
+                                                    mapping.UtcAccessExpirationDate,
+                                                    mapping.AccessTimeLimit.ToString());
+                                            }
+                                            else
+                                            {
+                                                invoiceItemId = _nexportService.AddItemToNexportOrderInvoice(invoiceId,
+                                                    mapping.NexportCatalogSyllabusLinkId.Value,
+                                                    CreateInvoiceItemRequest.ProductTypeEnum.Syllabus, productCost,
+                                                    subscriptionOrgId, groupMembershipIds,
+                                                    mapping.UtcAccessExpirationDate,
+                                                    mapping.AccessTimeLimit.ToString());
+                                            }
+
+                                            var nexportOrderInvoiceItem = new NexportOrderInvoiceItem()
+                                            {
+                                                OrderId = queueItem.OrderId,
+                                                OrderItemId = orderItem.Id,
+                                                InvoiceItemId = Guid.Parse(invoiceItemId),
+                                                UtcDateProcessed = DateTime.UtcNow
+                                            };
+
+                                            _nexportService.InsertNexportOrderInvoiceItem(nexportOrderInvoiceItem);
+
+                                            if (mapping.AutoRedeem)
+                                            {
+                                                redeemingOrderIds.Add(nexportOrderInvoiceItem.Id);
+                                            }
+
+                                            invoiceTotalCost += productCost;
+                                        }
+                                    }
+
+                                    //Add Payment
+                                    _nexportService.AddPaymentToNexportOrderInvoice(invoiceId, invoiceTotalCost,
+                                        userMapping.NexportUserId, queueItemId, DateTime.UtcNow);
+
+                                    // Commit
+                                    _nexportService.CommitNexportOrderInvoiceTransaction(invoiceId);
+
+                                    foreach (var nexportOrderInvoice in redeemingOrderIds
+                                        .Select(redeemingOrderId =>
+                                            _nexportService.FindNexportOrderInvoiceItemById(redeemingOrderId))
+                                        .Where(nexportOrderInvoice => nexportOrderInvoice != null))
+                                    {
+                                        _nexportService.RedeemNexportOrder(nexportOrderInvoice,
+                                            userMapping.NexportUserId);
+                                    }
+
+                                    _logger.Information($"Order {queueItem.OrderId} has been successfully processed!");
                                 }
-
-                                var nexportOrderInvoiceItem = new NexportOrderInvoiceItem()
+                                else
                                 {
-                                    OrderId = queueItem.OrderId,
-                                    OrderItemId = orderItem.Id,
-                                    InvoiceItemId = Guid.Parse(invoiceItemId),
-                                    UtcDateProcessed = DateTime.UtcNow
-                                };
+                                    var errMsg = $"The store {order.StoreId} could not be found for this order";
 
-                                _nexportService.InsertNexportOrderInvoiceItem(nexportOrderInvoiceItem);
-
-                                if (mapping.AutoRedeem)
-                                {
-                                    redeemingOrderIds.Add(nexportOrderInvoiceItem.Id);
+                                    var ex = new Exception(errMsg);
+                                    _logger.Error(errMsg, ex);
                                 }
+                            }
+                            else
+                            {
+                                var errMsg =
+                                    $"The user mapping for the customer {order.CustomerId} could not be found. The order cannot be processed until there is a proper user mapping.";
 
-                                invoiceTotalCost += productCost;
+                                var ex = new Exception(errMsg);
+                                _logger.Error(errMsg, ex);
                             }
                         }
-
-                        //Add Payment
-                        _nexportService.AddPaymentToNexportOrderInvoice(invoiceId, invoiceTotalCost, userMapping.NexportUserId, orderId, DateTime.UtcNow);
-
-                        // Commit
-                        _nexportService.CommitNexportOrderInvoiceTransaction(invoiceId);
-
-                        foreach (var nexportOrderInvoice in redeemingOrderIds.Select(redeemingOrderId => _nexportService.FindNexportOrderInvoiceItemById(redeemingOrderId)).Where(nexportOrderInvoice => nexportOrderInvoice != null))
+                        else
                         {
-                            _nexportService.RedeemNexportOrder(nexportOrderInvoice, userMapping.NexportUserId);
+                            _logger.Warning($"Cannot find the order {queueItem.OrderId}");
                         }
 
                         // Delete queue item
                         _nexportService.DeleteNexportOrderProcessingQueueItem(queueItem);
 
-                        _logger.Information($"Order {queueItem.OrderId} has been successfully processed!");
+                        _logger.Information($"Order processing queue item {queueItemId} has been processed and removed!");
                     }
                     catch (Exception ex)
                     {
-                        _logger.Error($"Cannot process the NexportRedemptionProcessingQueue item with Id {orderId}", ex);
+                        _logger.Error($"Cannot process the NexportRedemptionProcessingQueue item with Id {queueItemId}", ex);
                     }
                 }
             }
