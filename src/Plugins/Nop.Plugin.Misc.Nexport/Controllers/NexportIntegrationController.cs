fe257939b5688b6798cb36dd75c2d806442ee030
Joseph <joseph.stalling@nexportengineering.com>
2023-05-09 15:57:49 -0500

    Case 171217:
    removed caching from service method that gets the list of group membership mappings

--- src/Plugins/Nop.Plugin.Misc.Nexport/Controllers/NexportIntegrationController.cs
+++ src/Plugins/Nop.Plugin.Misc.Nexport/Controllers/NexportIntegrationController.cs
@@ -907,7 +907,6 @@ namespace Nop.Plugin.Misc.Nexport.Controllers
                         ViewBag.ClosePage = true;
                     }
 
-                    ViewBag.RefreshPage = true;
                 }
                 catch (Exception ex)
                 {
