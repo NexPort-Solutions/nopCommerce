1dc62eece7a17cfae5bec1b917be5f46f05ac9a7
Thai Nguyen <nguyenthai@live.com>
2025-10-17 15:37:37 -0500

    Update new marketplace build
    Case 216521: Fix query lookup for ordering store by name
    
    Case 216507: Fix custom registration field not validate inputs
    
    Misc: Adjust various resource strings

--- src/Plugins/Nop.Plugin.Misc.Nexport/Services/NexportStoreService.cs
+++ src/Plugins/Nop.Plugin.Misc.Nexport/Services/NexportStoreService.cs
@@ -1,27 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Nop.Core.Domain.Stores;
+﻿using Nop.Core.Domain.Stores;
 using Nop.Data;
 using Nop.Services.Stores;
 
 namespace Nop.Plugin.Misc.Nexport.Services;
 
-public class NexportStoreService : StoreService
+public class NexportStoreService(IRepository<Store> storeRepository) : StoreService(storeRepository)
 {
-    private readonly IRepository<Store> _storeRepository;
-
-    public NexportStoreService(IRepository<Store> storeRepository)
-        : base(storeRepository)
-    {
-        _storeRepository = storeRepository;
-    }
-
     public override async Task<IList<Store>> GetAllStoresAsync()
     {
-        var result = await _storeRepository.GetAllAsync(query => 
-            from s in query orderby s.Name, s.DisplayOrder, s.Id select s, cache => default);
-
-        return result;
+        return await _storeRepository.GetAllAsync(query =>
+        {
+            var result = from s in query orderby s.Name, s.DisplayOrder, s.Id select s;
+            return result;
+        }, includeDeleted: false);
     }
 }
